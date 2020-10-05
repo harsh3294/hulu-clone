df32534fe6fb733d5e39952db18fce32f185e73f
Harsh <harshc3294@gmail.com>
2020-10-05 14:01:14 +0530

    initial commit

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB7T2nYzElkUCBPKDQnlv9zRmOzRwOp3g8",
+    authDomain: "hulu-clone-f6347.firebaseapp.com",
+    databaseURL: "https://hulu-clone-f6347.firebaseio.com",
+    projectId: "hulu-clone-f6347",
+    storageBucket: "hulu-clone-f6347.appspot.com",
+    messagingSenderId: "692060561686",
+    appId: "1:692060561686:web:99c49e4ea7c7fb0bc63037",
+    measurementId: "G-G0F5JMPZ1S"
+  };
